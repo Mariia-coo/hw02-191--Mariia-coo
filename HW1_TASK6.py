431ec04008774eea5135d0a5c1d1d9bbc1118c80
Mariia-coo <46747277+Mariia-coo@users.noreply.github.com>
2019-10-21 00:06:37 +0300

    Add files via upload
    Домашняя работа 1 по курсу "программирование" на ФиПЛе.

---
+++ HW1_TASK6.py
@@ -0,0 +1,7 @@
+a= int(input('Чему равно число a?'))
+b= int(input ('Чему равно число b?'))
+c= int(input ('Чему равно число c?'))
+if (a**b==c):
+    print('a в степени b равно c')
+else:
+    print('a в степени b равно c')
---
+++ HW1_task1.py
@@ -0,0 +1,9 @@
+a= int(input('Чему равно число a?'))
+b= int(input ('Чему равно число b?'))
+c= int(input ('Чему равно число c?'))
+if (a+b>c) or (a+b<c):
+    print('Сумма a и b не равна c.')
+else:
+    print('Сумма a и b равна c.')
+
+       
